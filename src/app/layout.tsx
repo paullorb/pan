e55a150373ea230b94952660f18611ece890e007
Paul Lorber <paul.lorber@protonmail.com>
2024-09-09 18:59:19 +0200

    Refactor CSS to update background color and add favicon in authentication module and layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from './themeContext'; 
 import { HoursProvider } from './hoursContext';
+import Head from 'next/head'; // Import Head from next/head
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -8,9 +9,13 @@ interface RootLayoutProps {
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
-    <ThemeProvider> 
-      <HoursProvider> 
+    <ThemeProvider>
+      <HoursProvider>
         <html lang="en">
+          <Head>
+            <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><text y='20' font-size='20'>🍞</text></svg>" />
+            <title>Page Title</title>
+          </Head>
           <body>{children}</body>
         </html>
       </HoursProvider>
