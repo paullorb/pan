722c870add7a118b6e447434eb4e7f5154985f7a
Paul Lorber <paul.lorber@protonmail.com>
2024-09-10 19:19:14 +0200

    Refactor CSS to update padding and margin in authentication module and add background classes for hour component

--- src/app/components/hour.tsx
+++ src/app/components/hour.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useHours } from '../context/hoursContext';
 import Adjuster from './adjuster';
 import style from './hour.module.css';
@@ -11,63 +11,37 @@ const Hour: React.FC<HourProps> = ({ hour }) => {
   const { activities, handleActivityChange } = useHours();
   const activityFull = activities[`${hour}:00`] || '';
   const activityHalf = activities[`${hour}:30`] || '';
-  const nextActivityFull = activities[`${hour + 1}:00`] || '';
 
   const handleCopy = (sourceTime: string, destinationTime: string) => {
     handleActivityChange(destinationTime, activities[sourceTime] || '');
   };
 
-  const [isMerged, setIsMerged] = useState({
-    intoNext: false,
-    withinHour: false,
-    repeatedMiddle: false,
-  });
-
-  useEffect(() => {
-    setIsMerged({
-      intoNext:
-        activityFull === activityHalf &&
-        activityFull === nextActivityFull &&
-        activityFull !== '',
-      withinHour: activityFull === activityHalf && activityFull !== '',
-      repeatedMiddle:
-        activityFull === nextActivityFull &&
-        activityFull !== '' &&
-        (activities[`${hour - 1}:00`] === activityFull || activities[`${hour - 1}:30`] === activityFull),
-    });
-  }, [activityFull, activityHalf, nextActivityFull, activities, hour]);
-
-  // Determine background class based on the evenness of the hour
   const backgroundClass = hour % 2 === 0 ? style.altBackground1 : style.altBackground2;
-
-  if (isMerged.repeatedMiddle) return null;
 
   return (
     <div className={`${style.frame} ${backgroundClass}`}>
       <div className={`${style.hour} ${backgroundClass}`}>{hour}:00</div>
       <div className={style.events}>
-        <div className={`${style.pan} ${backgroundClass} ${isMerged.withinHour ? style.centered : ''}`}>
+        <div className={`${style.pan} ${backgroundClass}`}>
           <input
             type="text"
-            className={style.event}
+            className={`${style.event} ${backgroundClass}`}
             placeholder={`🍞 at ${hour}:00`}
             value={activityFull}
             onChange={(e) => handleActivityChange(`${hour}:00`, e.target.value)}
           />
-          <Adjuster onIncrease={() => handleCopy(`${hour}:00`, `${hour}:30`)} />
+          <Adjuster backgroundClass={backgroundClass} onIncrease={() => handleCopy(`${hour}:00`, `${hour}:30`)} />
         </div>
-        {!isMerged.withinHour && (
-          <div className={style.pan}>
-            <input
-              type="text"
-              className={style.event}
-              placeholder={`🍞 at ${hour}:30`}
-              value={activityHalf}
-              onChange={(e) => handleActivityChange(`${hour}:30`, e.target.value)}
-            />
-            <Adjuster onIncrease={() => handleCopy(`${hour}:30`, `${hour + 1}:00`)} />
-          </div>
-        )}
+        <div className={`${style.pan} ${backgroundClass}`}>
+          <input
+            type="text"
+            className={`${style.event} ${backgroundClass}`}
+            placeholder={`🍞 at ${hour}:30`}
+            value={activityHalf}
+            onChange={(e) => handleActivityChange(`${hour}:30`, e.target.value)}
+          />
+          <Adjuster backgroundClass={backgroundClass} onIncrease={() => handleCopy(`${hour}:30`, `${hour + 1}:00`)} />
+        </div>
       </div>
     </div>
   );
