8a4976919e9059aacf853a8625294220c5b32859
Paul Lorber <paul.lorber@protonmail.com>
2024-10-03 18:51:07 +0200

    Refactor CSS styles for hover effect in date component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,25 @@ export default function Home() {
   return (
     <div className={style.container}>
       <nav className={style.nav}>
+        <div className={style.leftUp}>
           <DateComponent />
+        </div>
+        <div className={style.rightUp}>
         <div className={style.titleC}>
           <h1 className={style.title}>
             <span className={style.emoji}>🍞 </span>Pansito del día
           </h1>
         </div>
           <Hamburger />
+        </div>
       </nav>
       <div className={style.tables}>
+        <div className={style.leftDown}>
         <Aside />
+        </div>
+        <div className={style.rightDown}>
         <Hours />
+        </div>
       </div>
   </div>
   );
