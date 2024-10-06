4ae6d966495eca8b4dfc118b55c27f8befaf6ca6
Paul Lorber <paul.lorber@protonmail.com>
2024-10-06 13:07:08 +0200

    Refactor file path for habit model

--- src/app/api/habits/route.ts
+++ src/app/api/habits/route.ts
@@ -2,7 +2,7 @@
 
 import { NextResponse } from 'next/server';
 import connectDB from '../../lib/mongodb';
-import Habit from '../../lib/models/Habit';
+import Habit from '../../lib/models/habit';
 import jwt from 'jsonwebtoken';
 
 export async function GET(request: Request) {
