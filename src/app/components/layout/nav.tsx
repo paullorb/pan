0a29b9a0623eb07873ec5ed9ee7d90d02e122f53
Paul Lorber <paul.lorber@protonmail.com>
2024-10-06 12:02:18 +0200

    Refactor nav component to improve layout and add date toggle

--- src/app/components/layout/nav.tsx
+++ src/app/components/layout/nav.tsx
@@ -1,11 +1,23 @@
 // /components/layout/nav.tsx
 
+import DateComponent from '../toggles/date/date';
+import Hamburger from './hamburger';
 import style from './nav.module.css';
 
 export default function Nav () {
   return (
-    <div className={style.container}>
-
-    </div>
+    <nav className={style.container}>
+      <div className={style.leftUp}>
+        <DateComponent  />
+      </div>
+      <div className={style.rightUp}>
+        <div className={style.titleC}>
+          <h1 className={style.title}>
+            <span className={style.emoji}>🍞 </span>Pansito del día
+          </h1>
+        </div>
+          <Hamburger />
+        </div>
+    </nav>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,14 @@
 
 import './aesthetics/globals.css';
 import style from './aesthetics/page.module.css';
-import Hamburger from './components/UI/hamburger/hamburger';
 import Aside from './components/layout/aside';
-import DateComponent from './components/toggles/date/date';
 import MainComponent from './components/layout/main';
+import Nav from './components/layout/nav';
 
 export default function Home() {
   return (
     <div className={style.container}>
-      <nav className={style.nav}>
-          <DateComponent />
-        <div className={style.rightUp}>
-        <div className={style.titleC}>
-          <h1 className={style.title}>
-            <span className={style.emoji}>🍞 </span>Pansito del día
-          </h1>
-        </div>
-          <Hamburger />
-        </div>
-      </nav>
+      <Nav />
       <div className={style.tables}>
         <Aside />
         <MainComponent />
