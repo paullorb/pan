5290d5a9a07b90a9e4b9cfb9cf53e348061e5810
Paul Lorber <paul.lorber@protonmail.com>
2024-10-27 15:30:32 +0100

    Refactor Header component to add toggle buttons and update layout

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { TogglesContext, TogglesState } from "../../context/togglesContext";
 import style from "./Header.module.css";
 
 const Header = () => {
   const context = useContext(TogglesContext);
+  const [mounted, setMounted] = useState(false);
+
   if (!context) {
     throw new Error("Header must be used within a TogglesProvider");
   }
 
   const { togglesState, setTogglesState } = context;
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const handleToggleClick = (toggleName: keyof TogglesState) => {
     setTogglesState((prevState) => ({
@@ -19,24 +25,37 @@ const Header = () => {
     }));
   };
 
-  return (
-    <header className={style.container}>
-      <div className={style.toggles}>
-        {(Object.keys(togglesState) as Array<keyof TogglesState>).map((toggle) => {
-          const buttonClass = togglesState[toggle] 
-            ? [style.toggle, style.active].join(' ')
-            : style.toggle;
-            
-          return (
+  const sortedToggles = Object.keys(togglesState).sort() as Array<keyof TogglesState>;
+
+  if (!mounted) {
+    return (
+      <header className={style.container}>
+        <div className={style.toggles}>
+          {sortedToggles.map((toggle) => (
             <button
               key={toggle}
-              onClick={() => handleToggleClick(toggle)}
-              className={buttonClass}
+              className={style.toggle}
             >
               {toggle.charAt(0).toUpperCase() + toggle.slice(1)}
             </button>
-          );
-        })}
+          ))}
+        </div>
+      </header>
+    );
+  }
+
+  return (
+    <header className={style.container}>
+      <div className={style.toggles}>
+        {sortedToggles.map((toggle) => (
+          <button
+            key={toggle}
+            onClick={() => handleToggleClick(toggle)}
+            className={togglesState[toggle] ? [style.toggle, style.active].join(' ') : style.toggle}
+          >
+            {toggle.charAt(0).toUpperCase() + toggle.slice(1)}
+          </button>
+        ))}
       </div>
     </header>
   );
