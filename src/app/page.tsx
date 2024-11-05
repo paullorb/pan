7b3387ef59575038b694beb6973d13857c295f26
Paul Lorber <paul.lorber@protonmail.com>
2024-11-05 17:50:57 +0100

    Fix import case for Page.module.css to ensure consistent styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // page.tsx
 
 import './aesthetics/globals.css';
-import style from './aesthetics/Page.module.css';
+import style from './aesthetics/page.module.css';
 import Aside from './components/layout/aside';
 import Header from './components/layout/Header';
 import MainComponent from './components/layout/main';
