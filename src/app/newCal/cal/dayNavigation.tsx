54643ff41e4677770a9eae1da62d6cfc3babac7d
Paul Lorber <paul.lorber@protonmail.com>
2025-02-13 19:00:45 +0100

    delete navigation features

--- src/app/newCal/cal/dayNavigation.tsx
+++ src/app/newCal/cal/dayNavigation.tsx
@@ -3,46 +3,27 @@
 "use client";
 import React from 'react';
 import styles from './calendar.module.css';
-import { formatDate, DAY_THRESHOLD } from './utils';
+import { formatDate } from './utils';
 import { useCalendar } from './calendarContext';
-import { useSwipeNavigation } from './useSwipeNavigation';
 
 const DayNavigation: React.FC = () => {
   const {
     selectedDate,
     setSelectedDate,
-    dayScrollAccum,
-    setDayScrollAccum,
-    handlePrevDay,
-    handleNextDay
   } = useCalendar();
 
   const handleReset = () => {
     setSelectedDate(new Date());
   };
 
-  const { onWheel, onTouchStart, onTouchMove, onTouchEnd } = useSwipeNavigation(
-    DAY_THRESHOLD,
-    dayScrollAccum,
-    setDayScrollAccum,
-    handlePrevDay,
-    handleNextDay
-  );
-
   return (
     <div className={styles.dayNavigation}>
-      <button onClick={handlePrevDay}>‹</button>
       <p
         className={styles.formattedDate}
         onClick={handleReset}
-        onWheel={onWheel}
-        onTouchStart={onTouchStart}
-        onTouchMove={onTouchMove}
-        onTouchEnd={onTouchEnd}
       >
         {formatDate(selectedDate)}
       </p>
-      <button onClick={handleNextDay}>›</button>
     </div>
   );
 };
--- src/app/newCal/cal/monthNavigation.tsx
+++ src/app/newCal/cal/monthNavigation.tsx
@@ -3,34 +3,18 @@
 "use client";
 import React from 'react';
 import styles from './calendar.module.css';
-import { getMonthName, MONTH_THRESHOLD } from './utils';
+import { getMonthName } from './utils';
 import { useCalendar } from './calendarContext';
-import { useSwipeNavigation } from './useSwipeNavigation';
 
 const MonthNavigation: React.FC = () => {
-  const { selectedDate, monthScrollAccum, setMonthScrollAccum, handlePrevMonth, handleNextMonth } = useCalendar();
+  const { selectedDate } = useCalendar();
 
-  const { onWheel, onTouchStart, onTouchMove, onTouchEnd } = useSwipeNavigation(
-    MONTH_THRESHOLD,
-    monthScrollAccum,
-    setMonthScrollAccum,
-    handlePrevMonth,
-    handleNextMonth
-  );
 
   return (
     <div className={styles.monthNavigation}>
-      <button onClick={handlePrevMonth}>‹</button>
-      <p
-        className={styles.monthName}
-        onWheel={onWheel}
-        onTouchStart={onTouchStart}
-        onTouchMove={onTouchMove}
-        onTouchEnd={onTouchEnd}
-      >
+      <p className={styles.monthName}>
         {getMonthName(selectedDate.getMonth())}
       </p>
-      <button onClick={handleNextMonth}>›</button>
     </div>
   );
 };
