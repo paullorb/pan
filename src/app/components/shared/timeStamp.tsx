e15ad6864cfdfad2b06330580a8687af7c18db6a
Paul Lorber <paul.lorber@protonmail.com>
2025-02-20 19:57:49 +0100

    1

--- src/app/components/shared/timeStamp.tsx
+++
@@ -1,9 +0,0 @@
-import style from './timeStamp.module.css';
-
-export default function TimeStamp () {
-  return (
-    <div className={style.container}>
-      <span>Timestamp: {new Date().toLocaleString()}</span>
-    </div>
-  )
-}
--- src/app/components/toggles/quote/quote.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './quote.module.scss';
-
-export default function Quote() {
-  return (
-    <div className={styles.container}>
-      <p>Quote</p>
-    </div>
-  );
-}
