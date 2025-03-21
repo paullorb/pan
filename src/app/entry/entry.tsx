502c704a7c32ffd116234baa308b24ba530be05d
Paul Lorber <paul.lorber@protonmail.com>
2025-03-21 21:17:17 +0100

    1

--- src/app/entry/entry.tsx
+++ src/app/entry/entry.tsx
@@ -1,15 +1,13 @@
-'use client'
-import React, { useState, useEffect, useMemo } from "react"
+"use client"
+import React, { useEffect, useMemo } from "react"
 import { useCalendar } from "../cal/calendarContext"
 import { useEntry } from "./entryContext"
 import styles from "./entry.module.css"
 import { getDateKey } from "./utils"
 import { useAuth } from "../auth/authContext"
 import EntryList from "./entryList"
-import Filter from "app/filter/filter"
 
 const Entry: React.FC = () => {
-  const [filter, setFilter] = useState<string | null>(null)
   const { selectedDate } = useCalendar()
   const { entries, fetchDayEntries } = useEntry()
   const { user } = useAuth()
@@ -20,21 +18,10 @@ const Entry: React.FC = () => {
   }, [keyDate, user, fetchDayEntries])
 
   const selectedEntries = entries[keyDate] || []
-  const totalCount = selectedEntries.length
-  const openCount = selectedEntries.filter((entry) => !entry.done).length
-  const filteredEntries =
-    filter === "open"
-      ? selectedEntries.filter((entry) => !entry.done)
-      : selectedEntries
 
   return (
     <div className={styles.container}>
-      <Filter
-        filter={filter}
-        totalCount={totalCount}
-        openCount={openCount}
-      />
-      <EntryList entries={filteredEntries} />
+      <EntryList entries={selectedEntries} />
     </div>
   )
 }
