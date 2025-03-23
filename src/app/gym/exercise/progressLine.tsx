54af44331edb9883a1bc5ba2bb5ce8843c78e5e4
Paul Lorber <paul.lorber@protonmail.com>
2025-03-23 15:40:35 +0100

    1

--- src/app/gym/exercise/progressLine.tsx
+++ src/app/gym/exercise/progressLine.tsx
@@ -1,19 +1,7 @@
 "use client"
-import { useWorkout } from "../workout/workoutContext"
-import { useRouter } from "next/navigation"
-import { slugify } from "./utils"
 import styles from "./progressLine.module.css"
 
 export default function ProgressLine() {
-  const { exercises, currentIndex } = useWorkout()
-  const router = useRouter()
-
-  const handleClick = (exercise: string) => {
-    router.push(`/gym/${slugify(exercise)}`)
-  }
-
-  const completedWidth =
-    exercises.length > 1 ? (currentIndex / (exercises.length - 1)) * 100 : 0
 
   return (
     <div className={styles.scrollContainer}>
@@ -22,28 +10,17 @@ export default function ProgressLine() {
           <div className={styles.lineBackground} />
           <div
             className={styles.lineCompleted}
-            style={{ width: `${completedWidth}%` }}
           />
         </div>
-        {exercises.map((exercise, index) => (
           <div
-            key={exercise}
             className={styles.item}
-            onClick={() => handleClick(exercise)}
           >
-            <div className={styles.label}>{exercise}</div>
             <div
-              className={
-                index < currentIndex
-                  ? styles.completedDot
-                  : index === currentIndex
-                  ? styles.currentDot
-                  : styles.futureDot
-              }
-            />
+              className={styles.itemContent}
+            >
+            </div>
           </div>
-        ))}
-      </div>
+    </div>
     </div>
   )
 }
