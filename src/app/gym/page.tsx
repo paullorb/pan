2352d3752585c00a38412fb71dc958404efdcb21
Paul Lorber <paul.lorber@protonmail.com>
2025-04-01 18:59:34 +0200

    1

--- src/app/gym/page.tsx
+++ src/app/gym/page.tsx
@@ -1,13 +1,13 @@
 // app/gym/page.tsx
 import Nav from "app/nav/nav"
 import styles from "./page.module.css"
-import Workout from "./workout/workout"
+import Card from "./exercise/card"
 
 export default function Page() {
   return (
     <div className={styles.container}>
       <Nav />
-      <Workout />
+      <Card />
     </div>
   )
 }
