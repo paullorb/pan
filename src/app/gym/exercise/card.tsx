07023006863e430d60f289a68d00a5d747d86d28
Paul Lorber <paul.lorber@protonmail.com>
2026-01-06 13:28:34 +0100

    fix: import React to ensure proper JSX handling

--- src/app/gym/exercise/card.tsx
+++ src/app/gym/exercise/card.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import styles from "./card.module.css"
 import exercises from "../exercises"
 import { useAuth } from "../../auth/authContext"
